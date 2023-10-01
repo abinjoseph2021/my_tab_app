f19b5bd0c474bee7f7994915256235cdcbc225b3
abinjoseph2050 <92216512+abinjoseph2050@users.noreply.github.com>
2023-10-01 12:55:44 +0530

    route update

--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab2Page } from './tab2.page';
+import { RouterModule, Routes } from '@angular/router';
+import { Tab5Page } from './tab2.page';
 
 const routes: Routes = [
   {
@@ -11,6 +13,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, RouterModule2]
 })
 export class Tab2PageRoutingModule {}
